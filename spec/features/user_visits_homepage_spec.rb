84ae9aed1539c34d9a6d83d461ed7ae5d39d0986
Justus <mj.kyalo@gmail.com>
2017-02-21 08:03:34 +0300

    add signin specs

--- spec/features/user_visits_homepage_spec.rb
+++ spec/features/user_visits_homepage_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature 'User visits homepage' do
   scenario 'Successfully' do
-    visit root_path
+    sign_in
     expect(page).to have_css 'h1', text: 'Todos'
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,28 +5,14 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'support/features/sign_in'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.use_transactional_fixtures = false
-
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
-  #
-  #     RSpec.describe UsersController, :type => :controller do
-  #       # ...
-  #     end
-  #
-  # The different available types are documented in the features, such as in
-  # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
-  # arbitrary gems may also be filtered via:
-  # config.filter_gems_from_backtrace("gem name")
+  config.include Features, type: :feature
 end
