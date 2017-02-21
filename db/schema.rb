7a612d781d438213260c2728fe5f0c5f5a85b1ba
Justus <mj.kyalo@gmail.com>
2017-02-21 10:24:55 +0300

    add new column to todos

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170217031049) do
+ActiveRecord::Schema.define(version: 20170221050408) do
 
   create_table "todos", force: :cascade do |t|
     t.text     "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "email"
   end
 
 end
